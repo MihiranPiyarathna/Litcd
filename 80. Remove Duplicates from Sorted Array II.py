5ce60f622242595a586289f5547ce1a7f32ade78
Mihiran Piyarathna <sasindumihiran@gmail.com>
2026-03-11 19:01:10 +0000

    message title
    optional message desc

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -66,20 +66,57 @@ if 999999 in nums:
 print( nums, k)
 # return k
 
-# """
-# Code sample from leet
+"""
+# idea from leet
+0(N) EASY TO UNDERSTAND SOLUTION
+
+Pratay Nandy
+50 Days Badge 2024
+80218
+Sep 19, 2023
+Array
+Two Pointers
+C++
+PLEASE UPVOTE MY SOLUTION IF YOU LIKE IT
+CONNECT WITH ME
+https://www.linkedin.com/in/pratay-nandy-9ba57b229/
+https://www.instagram.com/pratay_nandy/
+Approach
+The goal of this function is to remove duplicates from the nums vector while keeping at most two occurrences of any element. The function returns the length of the modified vector after removing duplicates.
+
+Here's the approach implemented in the code:
+
+Initialize an integer variable i to 0. This variable will keep track of the current position in the modified nums vector.
+
+Use a for loop to iterate through each element ele in the nums vector using the range-based for loop.
+
+Inside the loop, check the following conditions:
+
+i == 0: This condition ensures that the first element is always included in the modified vector.
+i == 1: This condition ensures that the second element is always included in the modified vector.
+nums[i-2] != ele: This condition checks if the current element ele is not the same as the element two positions before the current position i. This ensures that only two occurrences of any element are included in the modified vector.
+If any of the above conditions are met, copy the current element ele to the nums[i] position in the modified vector, and increment i by 1 to move to the next position.
+
+Repeat this process for all elements in the nums vector.
+
+Finally, return the value of i, which represents the length of the modified vector with duplicates removed.
+
+This approach effectively modifies the nums vector in place, removing duplicates while keeping at most two occurrences of each element. The function returns the length of the modified vector, which can be used to access the unique elements in the first i positions of the vector
+
+Complexity
+Time complexity:0(N)
+Space complexity:0(1)
+"""
+
+# nums = [0,0,1,1,1,1,2,3,3]
 # nums = [1,1,1]
 nums = [1,1,1,2,2,3]
-k = 2
-
-for i in range(2, len(nums)):
-    if nums[i] != nums[k-2]:
-        nums[k] = nums[i]
-        print( f"inside if: i-{i}, k-{k}, nums-{nums}")
-        k += 1
+wrt= 0
+for rdid in range( len(nums)):
+    if rdid ==0 or rdid==1 or nums[rdid]!=nums[wrt-2]: # note the use of wrt-2 instead of rdid
+        nums[wrt]= nums[rdid]
+        print("write to",wrt, nums)
+        wrt+=1
     else:
-        pass
-        print( f"inside else: i-{i}, k-{k}, nums-{nums}")
-# return k
-print( f"final: i-{i}, k-{k}, nums-{nums}")
-# """
+        print("not writing")   
+print(wrt, "to end consider ",nums)
